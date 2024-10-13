93ad5e1254e223cc04aec3b3c42bcac25e1e05e6
Farid Abulias <fabulias@MacBook-Pro-de-Farid.local>
2024-10-12 22:43:59 -0300

    feat: improve logic reducing to 1 while

--- merge-strings-alternately/main.py
+++ merge-strings-alternately/main.py
@@ -5,16 +5,13 @@ class Solution:
         iter1=0
         iter2=0
 
-        w_1=True
         a = []
         while iter1 < len1 or iter2 < len2:
             if iter1 < len1:
                 a.append(word1[iter1])
                 iter1+=1
-                w_1=False
             if iter2 < len2:
                 a.append(word2[iter2])
                 iter2+=1
-                w_1=True
 
         return ''.join(a)
